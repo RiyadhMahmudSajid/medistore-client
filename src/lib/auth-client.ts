bc3d588640ee27dc27e7c49abf12664d13c4013d
RiyadhMahmudSajid <rdhsajid@gmail.com>
2026-02-27 10:04:52 +0600

    final version

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     
-    baseURL: "http://localhost:5000"
+    baseURL: "https://medistore-server-o4zl.onrender.com"
 })

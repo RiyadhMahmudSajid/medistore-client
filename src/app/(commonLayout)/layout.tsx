1748e66002e5666c969e46070cc1f9ce355aefdd
RiyadhMahmudSajid <rdhsajid@gmail.com>
2026-03-05 01:01:36 +0600

    solve static and dynamic problem

--- src/app/(commonLayout)/layout.tsx
+++ src/app/(commonLayout)/layout.tsx
@@ -1,11 +1,15 @@
 import Navbar from "@/components/layout/navbar"
+import { Suspense } from "react"
 
 
 
 const layout = ({ children }: { children: React.ReactNode }) => {
    return (
      <div>
+      <Suspense fallback={<div className="h-16 bg-background animate-pulse" />}>
         <Navbar></Navbar>
+      
+      </Suspense>
        <main className="mt-16">
         {children}
       </main>
